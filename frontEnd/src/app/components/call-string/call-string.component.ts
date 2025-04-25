f94d2c1271775490aeba972ffa16172d173b3abf
jose23021995 <mtjamx95@gmail.com>
2025-04-24 20:16:46 -0600

    services

--- frontEnd/src/app/components/call-string/call-string.component.ts
+++ frontEnd/src/app/components/call-string/call-string.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { VozConfig, ResultadoReconocimiento} from '../../model/voz-config.model';
 
 @Component({
   selector: 'app-call-string',
