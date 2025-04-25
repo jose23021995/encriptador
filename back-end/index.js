8023f96ec36a73c6d1cd271b799adceada0fb6d3
jose23021995 <mtjamx95@gmail.com>
2025-04-24 18:01:18 -0600

    generacion de encriptado back end en el index.js

---
+++ back-end/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const crypto = require('crypto');
+const cors = require('cors'); // Importa el paquete cors
+
+const app = express();
+//app.use(cors()); // Habilita CORS para todas las rutas
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(bodyParser.json());
+
+// Carga la llave pública
+const publicKey = fs.readFileSync('public.pem', 'utf8');
+
+app.post('/encriptar', (req, res) => {
+  const texto = req.body.texto;
+
+  if (!texto) {
+    return res.status(400).json({ error: 'Falta el campo "texto".' });
+  }
+
+  const buffer = Buffer.from(texto, 'utf-8');
+
+  const encrypted = crypto.publicEncrypt(
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_PADDING,
+    },
+    buffer
+  );
+
+  const resultado = encrypted.toString('base64');
+
+  res.json({ encriptado: resultado });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor Express escuchando en http://localhost:3000');
+});
