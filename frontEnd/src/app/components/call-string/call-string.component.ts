b2760d160515002556f2086793e23d1168dd22bc
jose23021995 <mtjamx95@gmail.com>
2025-04-24 20:28:48 -0600

    componente de control de voz

--- frontEnd/src/app/components/call-string/call-string.component.ts
+++ frontEnd/src/app/components/call-string/call-string.component.ts
@@ -9,10 +9,72 @@ import { VozConfig, ResultadoReconocimiento} from '../../model/voz-config.model'
 
 @Component({
   selector: 'app-call-string',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './call-string.component.html',
   styleUrl: './call-string.component.css'
 })
-export class CallStringComponent {
+export class CallStringComponent implements OnInit{
+  @Input() config!: VozConfig;
 
+  @Output() reconocimientoFinalizado = new EventEmitter<ResultadoReconocimiento>();
+
+  textoReconocido: string = '';
+  escuchando: boolean = false;
+
+  private reconocimiento: any;
+
+  constructor(private zone: NgZone) {}
+  ngOnInit(): void {
+    const SpeechRecognition =
+      (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+
+    if (!SpeechRecognition) {
+      console.error('El navegador no soporta Web Speech API');
+      return;
+    }
+
+    this.reconocimiento = new SpeechRecognition();
+    this.reconocimiento.lang = 'es-MX';
+    this.reconocimiento.continuous = true;
+    this.reconocimiento.interimResults = true;
+
+    this.reconocimiento.onresult = (event: any) => {
+      let transcript = '';
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        transcript += event.results[i][0].transcript;
+      }
+
+      this.zone.run(() => {
+        this.textoReconocido = transcript.slice(0, this.config.length);
+      });
+    };
+
+    this.reconocimiento.onend = () => {
+      this.escuchando = false;
+      this.reconocimientoFinalizado.emit({
+        texto: this.textoReconocido.trim(),
+        fecha: new Date(),
+        idioma: 'es-MX'
+      });
+    };
+  }
+
+  toggleEscucha(): void {
+    if (!this.reconocimiento) return;
+
+    if (this.escuchando) {
+      this.reconocimiento.stop();
+    } else {
+      this.reconocimiento.start();
+    }
+    this.escuchando = !this.escuchando;
+  }
 }
