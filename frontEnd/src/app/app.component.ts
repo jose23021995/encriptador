3bf94990e2bce39d12a64b783d41431ecd5c689e
jose23021995 <mtjamx95@gmail.com>
2025-04-24 20:55:50 -0600

    componente app-component

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EncriptadorService } from './utilidades/services/encriptador.service';
+import { EncryptionFormComponent } from "./components/encryption-form/encryption-form.component"; 
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EncryptionFormComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontEnd';
+  constructor(){}
 }
